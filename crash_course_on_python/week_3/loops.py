ef8a1a802b6259bed061c503e9f8dbb90fcf2c83
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-15 16:06:31 -0600

    Update loops.py

--- crash_course_on_python/week_3/loops.py
+++ crash_course_on_python/week_3/loops.py
@@ -6,13 +6,14 @@
 # What sets a while loop apart, however, is that this code block will keep executing as long as the evaluation statement is true. 
 # Once the statement is no longer true, the loop exits and the next line of code will be executed.  
 
+###############################
 x = 0
 while x < 5:
   print("Not there yet, x=" + str(x))
   x = x + 1
 print("x=" + str(x))
 
-
+##################################
 def attempts(n):
     x = 1
     while x <= n:
@@ -21,3 +22,30 @@ def attempts(n):
     print("Done")
     
 attempts(10)
+
+####################################
+username = get_username()
+while not valid_username(username):
+  print("Invalid username")
+  username = get_username()
+  
+#------------------------------------------------------------------------------------
+# Common Pitfalls With Variable Initialization
+# You'll want to watch out for a common mistake: forgetting to initialize variables. If you try to use a variable without first initializing it, you'll run into a NameError. 
+# This is the Python interpreter catching the mistake and telling you that you’re using an undefined variable. 
+# The fix is pretty simple: initialize the variable by assigning the variable a value before you use it.
+
+# Another common mistake to watch out for that can be a little trickier to spot is forgetting to initialize variables with the correct value. 
+# If you use a variable earlier in your code and then reuse it later in a loop without first setting the value to something you want, 
+# your code may wind up doing something you didn't expect. Don't forget to initialize your variables before using them!
+  ###################################
+  def count_down(start_number):
+    current=3
+    while (current > 0):
+      print(current)
+      current -= 1
+    print("Zero!")
+    
+
+
+count_down(3)

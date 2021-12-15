1d5bff995cb74ff5cadd7f785229a7362033c4fb
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-15 15:50:21 -0600

    Update loops.py

--- crash_course_on_python/week_3/loops.py
+++ crash_course_on_python/week_3/loops.py
@@ -11,3 +11,13 @@ while x < 5:
   print("Not there yet, x=" + str(x))
   x = x + 1
 print("x=" + str(x))
+
+
+def attempts(n):
+    x = 1
+    while x <= n:
+        print("Attempt " + str(x))
+        x += 1
+    print("Done")
+    
+attempts(10)

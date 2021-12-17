50415e49b6a2834a36c85501b677fed0f6e7c7a5
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-16 22:21:14 -0600

    Update for_loops.py

--- crash_course_on_python/week_3/for_loops.py
+++ crash_course_on_python/week_3/for_loops.py
@@ -52,6 +52,35 @@ print(factorial(5)) # should return 120
 # Three parameters will create a sequence starting with the first parameter and stopping before the second parameter, but this time increasing each step by the third parameter.
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-## 
+## Nested Loops
 
+###################################
+
+for left in range(7):
+  for right in range(left, 7):
+    print("[" + str(left) + "|" + str(right) + "]", end = " ")
+  print()
+  
+###################################
+teams = [ 'Dragons', 'Wolves', 'Pandas', 'Unicorns']
+
+for home_teams in teams:
+  for away_team in teams:
+    if home_team != away_team:
+      print(home_team + " vs " + away_teen)
+      
+ ###################################
+# The validate_users function is used by the system to check if a list of users is valid or invalid. 
+# A valid user is one that is at least 3 characters long. For example, ['taylor', 'luisa', 'jamaal'] are all valid users. 
+# When calling it like in this example, something is not right. Can you figure out what to fix?
+
+def validate_users(users):
+  for user in users:
+    if is_valid(user):
+      print(user + " is valid")
+    else:
+      print(user + " is invalid")
+
+validate_users(["purplecat"])
+     
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------

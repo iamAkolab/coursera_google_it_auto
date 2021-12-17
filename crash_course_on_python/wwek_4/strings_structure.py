64f027e970427f686b03dc8784635e0d57f47061
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-17 01:14:14 -0600

    Create strings_structure.py

---
+++ crash_course_on_python/wwek_4/strings_structure.py
@@ -0,0 +1,16 @@
+## Strings
+
+'''
+Modify the double_word function so that it returns the same word repeated twice, followed by the length of the new doubled word. 
+For example, double_word("hello") should return hellohello10.
+'''
+def double_word(word):
+#    if(word == ""):
+#      return 0
+#    else:
+      doubleWord = word + word 
+      return  str(doubleWord) + str(len(doubleWord))
+
+print(double_word("hello")) # Should return hellohello10
+print(double_word("abc"))   # Should return abcabc6
+print(double_word(""))      # Should return 0

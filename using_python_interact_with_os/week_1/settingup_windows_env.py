c0d6bef64d106fbf9f55d5f5a40623476a7ab045
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-23 04:37:33 -0600

    Create settingup_windows_env.py

---
+++ using_python_interact_with_os/week_1/settingup_windows_env.py
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## In powerShell
+'''
+
+PS C:\WINDOWS\system32> python --version
+Python 3.9.0
+
+PS C:\WINDOWS\system32> python
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+
+>>> import requests
+
+>>> response = requests.get("https://www.google.com")
+
+>>> len(response.text)
+15084
+
+'''

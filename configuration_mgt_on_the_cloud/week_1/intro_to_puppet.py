527dcb259725f741d02480f8d2acd8d73d89577f
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-25 17:48:21 -0600

    Update intro_to_puppet.py

--- configuration_mgt_on_the_cloud/week_1/intro_to_puppet.py
+++ configuration_mgt_on_the_cloud/week_1/intro_to_puppet.py
@@ -35,3 +35,44 @@ for us. We can add, remove, or modify configuration files stored in the system, 
 that run on our computer. We can configure crone jobs, the scheduled tasks, add, remove, or modify Users and Groups or even execute external commands, if that's what we need
 '''
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## Puppet Resources
+
+# The most basic unit in Puppet is a resource, such as user, group, file, service or package.
+# In other words, each resource specifies one configuration that we're trying to manage, like a service, a package, or a file. 
+
+# Let's look at another example. In this case, we're defining a file resource
+
+class sysctl {
+   # make sure the directory exists, some distros dont have it
+   file { '/etc/sysctl.d':
+         ensure => directory,
+        }
+}
+
+# In this case, it's a very simple rule that ensures that etc/sysctl.d exists and is a directory. Let's talk a little bit about syntax. In both our last example and this one we 
+# could see that when declaring a resource in puppet, we write them in a block that starts with the resource type ,in this case File. The configuration of the resource is then 
+# written inside a block of curly braces. Right after the opening curly brace, we have the title of the resource, followed by a colon. After the colon come the attributes that 
+# we want to set for the resource. In this example, we're once again setting the insurer attribute with directory as the value, but we could set other attributes too
+
+# Let's check out a different file resource
+claas timezone { 
+      
+   file { '/etc/timezone':
+         ensure => file,
+         content => "UTC/n"
+         replace => true,
+}
+   
+# In this example, we're using a file resource to configure the contents of etc/timezone, a file, that's used in some Linux distributions to determine the time zone of the 
+# computer. This resource has three attributes. First, we explicitly say that this will be a file instead of a directory or a symlink then we set the contents of the file 
+# to the UTC time zone. Finally, we set the replace attribute to true, which means that the contents of the file will be replaced even if the file already exists. 
+# We've now seen a couple examples of what we can do with the file resource. There are a lot more attributes that we could set, like file permissions the file owner, or the filemodification time. 
+
+# How do these rules turn into changes in our computers? When we declare a resource in our puppet rules. We're defining the desired state of that resource in the system. 
+# The puppet agent then turns the desired state into reality using providers.
+# The provider used will depend on the resource defined and the environment where the agent is running. Puppet will normally detect this automatically without us having to do 
+# anything special. When the puppet agent processes a resource, it first decides which provider it needs to use, then passes along the attributes that we configured in the 
+# resource to that provider. The code of each provider is in charge of making our computer reflect the state requested in the resource
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## Puppet Classes
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------

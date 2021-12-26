0c67b4d4bdc6de40688fc953bc7849d3abb684bd
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-26 03:41:44 -0600

    Update qwikslabs_debugging_puppet_installation.py

--- configuration_mgt_on_the_cloud/week_1/qwikslabs_debugging_puppet_installation.py
+++ configuration_mgt_on_the_cloud/week_1/qwikslabs_debugging_puppet_installation.py
@@ -108,4 +108,73 @@ $ sudo nano init.pp
 # After that, you can trigger a manual run of the Puppet agent by running the following command:
 
 $ sudo puppet agent -v --test 
+
+# Go ahead and initiate a second SSH connection following the same instructions from the Accessing the linux virtual machine section. Then, verify that the profile is 
+# fixed by checking the contents of the PATH variable.
+
+# To check the PATH variable has the right contents, use the command below in the second SSH connection:
+$ echo $PATH
+
+# you've successfully fixed the misbehaving $PATH variable.
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+'''
+student-02-4c5016a37ae5@puppet:~$ echo $PATH
+/java/bin:/snap/bin
+student-02-4c5016a37ae5@puppet:~$ ls /
+Command 'ls' is available in '/bin/ls'
+The command could not be located because '/bin' is not included in the PATH environment variable.
+ls: command not found
+student-02-4c5016a37ae5@puppet:~$ export PATH=/bin:/usr/bin
+student-02-4c5016a37ae5@puppet:~$ ls
+student-02-4c5016a37ae5@puppet:~$ ls /
+bin   dev  home        initrd.img.old  lib64       media  opt   root  sbin  srv  tmp  var      vmlinuz.old
+boot  etc  initrd.img  lib             lost+found  mnt    proc  run   snap  sys  usr  vmlinuz
+student-02-4c5016a37ae5@puppet:~$ cd /etc/puppet/code/environments/production/modules/profile/manifests
+student-02-4c5016a37ae5@puppet:/etc/puppet/code/environments/production/modules/profile/manifests$ cat init.php
+cat: init.php: No such file or directory
+student-02-4c5016a37ae5@puppet:/etc/puppet/code/environments/production/modules/profile/manifests$ cat init.pp
+class profile {
+        file { '/etc/profile.d/append-path.sh':
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0646',
+                content => "PATH=/java/bin\n",
+        }
+}
+student-02-4c5016a37ae5@puppet:/etc/puppet/code/environments/production/modules/profile/manifests$ sudo nano init.pp
+student-02-4c5016a37ae5@puppet:/etc/puppet/code/environments/production/modules/profile/manifests$ sudo puppet agent -v --test
+Info: Using configured environment 'production'
+Info: Retrieving pluginfacts
+Info: Retrieving plugin
+Info: Retrieving locales
+Info: Caching catalog for puppet.us-central1-a.c.qwiklabs-gcp-03-00ee7a2c7434.internal
+Info: Applying configuration version '1640511609'
+Notice: /Stage[main]/Profile/File[/etc/profile.d/append-path.sh]/content:
+--- /etc/profile.d/append-path.sh       2021-12-26 08:53:32.109808886 +0000
++++ /tmp/puppet-file20211226-7732-6ng9oi        2021-12-26 09:40:09.493155513 +0000
+@@ -1 +1 @@
+-PATH=/java/bin
++PATH=$PATH:/java/bin
+
+Info: Computing checksum on file /etc/profile.d/append-path.sh
+Info: /Stage[main]/Profile/File[/etc/profile.d/append-path.sh]: Filebucketed /etc/profile.d/append-path.sh to puppet with sum b1ca50e93b6ea92bd7b3eeb02c69f109
+Notice: /Stage[main]/Profile/File[/etc/profile.d/append-path.sh]/content: content changed '{md5}b1ca50e93b6ea92bd7b3eeb02c69f109' to '{md5}30a2bc8929446ce04807cd3c5921cb14'
+Notice: Applied catalog in 0.08 seconds
+student-02-4c5016a37ae5@puppet:/etc/puppet/code/environments/production/modules/profile/manifests$ echo $PATH
+/bin:/usr/bin
+student-02-4c5016a37ae5@puppet:/etc/puppet/code/environments/production/modules/profile/manifests$
+student-02-4c5016a37ae5@puppet:/etc/puppet/code/environments/production/modules/profile/manifests$ student-02-4c5016a37ae5@puppet:~$ echo $PATH
++++ /tmp/puppet-file20211226-7732-6ng9oi        2021-12-26 09:40:09.493155513 +0000
+@@ -1 +1 @@
+-PATH=/java/bin
++PATH=$PATH:/java/bin
+
+Info: Computing checksum on file /etc/profile.d/append-path.sh
+Info: /Stage[main]/Profile/File[/etc/profile.d/append-path.sh]: Filebucketed /etc/profile.d/append-path.sh to puppet with sum b1ca50e93b6ea92bd7b3eeb02c69f109
+Notice: /Stage[main]/Profile/File[/etc/profile.d/append-path.sh]/content: content changed '{md5}b1ca50e93b6ea92bd7b3eeb02c69f109' to '{md5}30a2bc8929446ce04807cd3c5921cb14'
+Notice: Applied catalog in 0.08 seconds
+student-02-4c5016a37ae5@puppet:/etc/puppet/code/environments/production/modules/profile/manifests$ echo $PATH
+/bin:/usr/bin
+
+'''
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------

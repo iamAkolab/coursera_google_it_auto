b06ea516714890882b527a37262aa61f576dc3b4
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-27 20:34:14 -0600

    Update reading_writing_csv.py

--- using_python_interact_with_os/week_2/reading_writing_csv.py
+++ using_python_interact_with_os/week_2/reading_writing_csv.py
@@ -69,9 +69,72 @@ with open('host.csv', 'w') as hosts_csv:
          
  $ cat host.csv
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-##
+## Reading and Writing CSV Files with Dictionaries
 
+# In our earlier examples, we saw how we can read and write CSV files, and we use list as datatype on the Python side. This works when we know what the fields are going to be, 
+# but it can be pretty cumbersome when we have a lot of columns, and we need to remember which is which. Imagine if your lists of employees not only had name, phone number and 
+# role but also start date, username, office location, department, preferred pronouns and so on. It would soon get hard to keep track of which column corresponds to which 
+# position in the row. For cases like this, it's common for CSVs to include the names of the columns as a first line in the file, like in this example; this CSV file list a 
+# bunch of internally developed programs used at the company including the latest version, the current development status and the number of people using it. Check out how the 
+# first line of the file includes the names of each of the fields
+         
+$ cat software.csv
+ # name, version. status, users
+ # MailTree, 5.34, production, 324
+ # CalDoor, 1.25, beta, 22
+ # Chatty Chicken, 0.34, alpha, 4
+ 
+# We can profit from this additional information by using DictReader, a slightly different reader that's also provided by the CSV module. This reader turns each row of the data
+# in a CSV file into a dictionary. We can then access the data by using the column names instead of the position in the row. Let's see how that looks. 
+
+with open('software.csv') as software:
+         reader = csv.DictReader(software)
+         for row in reader:
+             print(("{} has {} users").format(row["Name"], rows["users"]))
+         
+# MailTree has 324 users
+# CalDoor has 22 users
+# Chatty Chickens has 4 users
+         
+
+# So here we're opening the  file and creating a DictReader to process our CSV data, then when going through the rows we can access information in each row using the keys just 
+# like we would when accessing data in the dictionary. See how we use row, name and row users to get the name of the number of users.
+         
+# we can use DictWriter in a similar way to generate a CSV file from the contents of a list of dictionaries. This means that each element in the list will be a row in the file, 
+# and the values of each field will come out of each of the dictionaries. For this to work, we'll also need to pass a list of the keys that we want to be stored in the file 
+# when creating the writer. Let's see this in action.
+         
+ # First we need a list of dictionaries with the data that we want to store. For this example, we want to store data about the users in our company and the departments that 
+ # they work in. So here we have our list of dictionaries and each contain the keys, name, username and department.
+  
+users = [ {"name": "Sol Mansi", "username": "solm", "separtment": "IT Infrastructure"}, 
+          {"name": "Sol Mansi", "username": "solm", "separtment": "IT Infrastructure"},
+          {"name": "Sol Mansi", "username": "solm", "separtment": "IT Infrastructure"},]
+   
+# So we first define the list of keys that we want to write to the file       
+keys = ["name", "username", "deparemnt"]
+   
+ # then we open the file for writing. Next we created the DictWriter passing the keys that we had identified before, and then we call two different methods on the writer. 
+ # The right header method will create the first line of the CSV based on keys that we passed, and the right rows method will turn the list of dictionaries into lines in that file  
+         
+ with open('by_department.csv', 'w') as by_department:
+         write = csv.DictWriter(by_department, fieldnames = keys)
+         write.writeheader()
+         writer.writerows(users)
+         
+# To check if it works correctly 
+$ cat by_department.csv
+         
+# You can read, write and work with files and directories, and now, you can also read and write CSV files      
+         
+         
+# DictReader() allows us to convert the data in a CSV file into a standard dictionary. DictWriter() \ allows us to write data from a dictionary into a CSV file. 
+# What’s one parameter we must pass in order for DictWriter() to write our dictionary to CSV format? The fieldnames parameter of DictWriter() requires a list of keys
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-##
+## CSV Files Cheat Sheet
+
+# Check out the following links for more information:
+# https://docs.python.org/3/library/csv.html
+# https://realpython.com/python-csv/
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------

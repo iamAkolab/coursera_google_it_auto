c803d4cac6527034ac9cdfee0edca9bc10959c0f
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-01-03 00:13:12 -0600

    Update basic_regular_expression.py

--- using_python_interact_with_os/week_3/basic_regular_expression.py
+++ using_python_interact_with_os/week_3/basic_regular_expression.py
@@ -1,6 +1,26 @@
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ##
 
+# As we called it out before, we use the re module to apply regular expressions in Python. This module includes a bunch of different functions that can help manipulate strings. 
+# Let's see how we can use this module for some basic matching
+
+import re
+
+result = re.search(r"aza", "plaza")
+
+# We call the search function on the re module, and told it to use the pattern aza on the string plaza. We then stored the return value of that function in the result variable. 
+# The r at the beginning of the pattern indicates that this is a rawstring. This means that Python interpreter shouldn't try to interpret any special characters, and instead,
+# should just pass the string to the function as is. In this example, there are no special characters. The rawstring and the normal string are exactly the same, but it's a good
+# idea to always use rawstrings for regular expressions in Python
+
+# let's print the result of the variable and see what we get.
+print(result)
+# <re.Match object; span=(2,5), match='aza'>
+
+# Great. Our result is a match object. The output we get when calling print already shows some interesting information, like the position in the string that matched ,and what 
+# the actual matching string was. Let's try this out again with different word.
+
+# 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ##
 

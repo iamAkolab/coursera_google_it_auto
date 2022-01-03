f4970c9b0e18d14b81e3794f082a19281b783523
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-01-02 23:47:02 -0600

    Update basic_regular_expression.py

--- using_python_interact_with_os/week_3/basic_regular_expression.py
+++ using_python_interact_with_os/week_3/basic_regular_expression.py
@@ -1,1 +1,19 @@
 
+# Fill in the code to check if the text passed contains the vowels a, e and i, with exactly one occurrence of any other character in between
+import re
+def check_aei (text):
+  result = re.search(r"a.e.i", text)
+  return result != None
+
+print(check_aei("academia")) # True
+print(check_aei("aerial")) # False
+print(check_aei("paramedic")) # True
+
+'''
+Here is your output:
+True
+False
+True
+
+Great work! You've written your first regular expression!
+''

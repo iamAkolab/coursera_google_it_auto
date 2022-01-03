5c15cdd1f452ec28c52e1d47d9ddf7ed3fcf8209
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-01-03 01:26:53 -0600

    Update basic_regular_expression.py

--- using_python_interact_with_os/week_3/basic_regular_expression.py
+++ using_python_interact_with_os/week_3/basic_regular_expression.py
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-##
+## Simple Matching in Python
 
 # As we called it out before, we use the re module to apply regular expressions in Python. This module includes a bunch of different functions that can help manipulate strings. 
 # Let's see how we can use this module for some basic matching
@@ -69,8 +69,6 @@ print(re.search(r"p.ng", "clapping"))
 # For example, if we want our match to be case insensitive, we can do this by passing the re.IGNORECASE option
 print(re.search(r"p.ng", "Pangaea", re.IGNORECASE))
 # <re.Match object; span=(4,8), match='Pang'>
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-##
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## QUESTION 1
@@ -92,4 +90,98 @@ True
 
 Great work! You've written your first regular expression!
 '''
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## Wildcards and Character Classes
+# We've seen by now how we can use a dot in our regular expressions as a special character that can match any character. In the regex world, this is known as a wildcard because 
+# it can match more than one character. Using a dot is the broadest possible wildcard because it matches absolutely any character. But what if we wanted something stricter, 
+# to restrict our wildcards to a range of characters to do this. For this task we use another feature of regexes called character classes. Character classes are written inside 
+# square brackets and let us list the characters we want to match inside of those brackets. For example, say we want to match the word Python but allow for both lowercase or 
+# uppercase p. We could do it like this.
+
+print(re.search(r"[a-z]way", "The end of the highway"))
+# <re.Match object; span=(18,22), match='hway'>
+
+# Inside the square brackets, we can also define a range of characters using a dash. For example, we could use lowercase a to lowercase z to state any lowercase letter. So if we 
+# wanted to look for the string way preceded by any letter, we could write the expression like this one
+
+print(re.search(r"[a-z]way", "What a way to go"))
+# None
+
+# In this case, we didn't get a match. That's because the string way is preceded by a space and that doesn't match the range that we defined. We can define more ranges like upper 
+# case A to upper case Z for all upper case letters or 0 to 9 for all digits. We can combine as many ranges and symbols as we want, like this.
+
+print(re.search(r"cloud[a-zA-Z0-9]", "cloudy"))
+# <re.Match object; span=(0,6), match='cloudy'>
+
+print(re.search(r"cloud[a-zA-Z0-9]", "cloud9"))
+# <re.Match object; span=(0,6), match='cloud9'>
+
+# Sometimes we may want to match any characters that aren't in a group. To do that, we use a circumflex inside the square brackets. For example, let's create a search pattern 
+# that looks for any characters that's not a letter.
+
+print(re.search(r"[^a-zA-Z]", "This is a sentnence with spaces."))
+# <re.Match object; span=(4,5), match=' '>
+# Here, our pattern matched the first space in the sentence. What if we also add a space to the list of characters that we don't want to match?
+
+print(re.search(r"[^a-zA-Z ]", "This is a sentnence with spaces."))
+# <re.Match object; span=(30,31), match='.'>
+# Because we added a space inside the character class, our example now matched the final dot in the sentence, which isn't in the list of characters to exclude. 
+
+#If we want to match either one expression or another, we can use the pipe symbol to do that. This lets us list alternative options that can get matched. 
+# For example, we could have an expression that matches either the word cat or the word dog, like this.
+
+print(re.search(r"cat|dog", "I like cats."))
+# <re.Match object; span=(7,10), match='cat'>
+
+# So since the string contains the sub string cat, the search function could find it. Let's try an example with dog.
+
+print(re.search(r"cat|dog", "I like dogs."))
+# <re.Match object; span=(7,10), match='dog'>
+
+# This expression contains a sub string dog, so we found a match. Let's try a sentence with both dogs and cats
+
+print(re.search(r"cat|dog", "I like dogs and cat."))
+# <re.Match object; span=(7,10), match='dog'>
+
+# In this string, we actually have two possible matches for our search. But we only get the first one. That's the way the search function works. If we want to get all possible 
+# matches, we can do that using the findall function, which is also provided by the re module, like this.
+
+print(re.findall(r"cat|dog", "I like dogs and cat."))
+# ['dog','cat']
+
+# So with findall, we don't need to choose between cats and dogs. What a relief.
+# These tools can be super useful in the IT world, so you'll definitely want to get a good handle on them. 
+# Now, what do you think we'd have to do if we wanted to match something more than once?
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## QUESTION 2
+# Fill in the code to check if the text passed contains punctuation symbols: commas, periods, colons, semicolons, question marks, and exclamation points.
+
+import re
+def check_punctuation (text):
+  result = re.search(r"[,.:;?!]", text)
+  return result != None
+
+print(check_punctuation("This is a sentence that ends with a period.")) # True
+print(check_punctuation("This is a sentence fragment without a period")) # False
+print(check_punctuation("Aren't regular expressions awesome?")) # True
+print(check_punctuation("Wow! We're really picking up some steam now!")) # True
+print(check_punctuation("End of the line")) # False
+
+'''
+Here is your output:
+True
+False
+True
+True
+False
+
+Right on! You're seeing the flexibility of character classes
+in regular expressions!
+'''
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+##
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+##
+
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
